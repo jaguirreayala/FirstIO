5e5ff422e254844d1f1395076d0c3f9adfff5143
jaguirreayala <jaguirre-ayala@mail.csuchico.edu>
2018-09-05 17:17:41 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+//Author: Juan Aguirre-Ayala
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Juan. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
